5ac99917fa988fad948c79de81c328b43dbd58a2
Daisy <li.daisy2003@gmail.com>
2022-07-23 09:26:18 -0400

    Update goToTims.cc

--- goToTims.cc
+++ goToTims.cc
@@ -1,6 +1,6 @@
 #include "goToTims.h"
 
-GoToTims::GoToTims(int bi, bool prop, Square& tims) : Square{bi}, tims(tims) {}
+GoToTims::GoToTims(int bi, bool prop, Square& tims) : Square{bi, prop}, tims(tims) {}
 
 void GoToTims::land(Player p) {
     p.changePos(this->getBoardIndex());
@@ -8,7 +8,6 @@ void GoToTims::land(Player p) {
     struct State s;
     s.notifType = StateType::Landed;
     s.justLanded = &p;
-    s.owner = this->getOwner();
     s.property = PropertyType::Special;
     this->setState(s);
 
