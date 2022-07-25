e3fab373e7b18ca480c962191f24fdbd9afe4ece
Zheng Qiu <77452278+ZhengQ2@users.noreply.github.com>
2022-07-25 10:47:13 -0400

    Update player.cc

--- player.cc
+++ player.cc
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iostream>
 
-Player::Player(std::string n, char p): name{n}, piece{p} {}
+Player::Player(std::string n, char p): piece{p}, name{n} {}
 
 int Player::getPos() {
     return position;
