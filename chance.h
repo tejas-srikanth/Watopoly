ab246a1ea7fa1a291d6b630223b1849ec217f686
Zheng Qiu <77452278+ZhengQ2@users.noreply.github.com>
2022-07-25 14:14:37 -0400

    Update chance.h

--- chance.h
+++ chance.h
@@ -3,10 +3,13 @@
 #include <vector>
 #include "square.h"
 #include "player.h"
+#include "dcTims.h"
 
 class Chance : public Square {
+    protected:
+    DCTims& tims;
     public:
-    Chance(int bi, string name);
+    Chance(int bi, string name, DCTims& tims);
     void land(Player& p) override;
     virtual void getEffect(Player &p);
 };
