ab4ecb393782b4a91a0a6d093af5f75fd159f09f
Daisy <li.daisy2003@gmail.com>
2022-07-26 16:36:19 -0400

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,15 +1,19 @@
 #include "board.h"
 
-int main(){
-    cout << "How many players will we have: ";
-    int x;
-    cin >> x;
-    while ( x > 6) {
-        cout << "You cannot have more than 6 players." <<endl;
+int main(int argc, char* argv[]){
+    if (argc == 0) {
         cout << "How many players will we have: ";
+        int x;
         cin >> x;
+        while ( x > 6) {
+            cout << "You cannot have more than 6 players." <<endl;
+            cout << "How many players will we have: ";
+            cin >> x;
+        }
+        Board b{x};
+        b.initialize();
+        b.play();
     }
-    Board b{x};
-    b.initialize();
-    b.play();
+    
+    
 }
